fe2acea20dc94e2afbe7fef85aab291a0cbe1ada
suvinay17 <suvinay.bothra@richmond.edu>
2020-11-28 22:53:07 -0500

    Both feature vectors done other than ngram and character ngram

--- Vectorizer.py
+++ Vectorizer.py
@@ -8,6 +8,7 @@ class Vectorizer():
         self.tweets = tweets
         pass
 
+
     def getNgram(self, n):
         g = {}
         for tweet in self.tweets:
@@ -35,6 +36,7 @@ class Vectorizer():
                 g[s] += 1 if s in g else 1
                 return g
 
+
     def generateNgramID(self, n, type):
         ind = 0
         ngrams = getNgram(n) if type == 0 else getCharNgram(n)
@@ -54,18 +56,36 @@ class Vectorizer():
         for i in range(len(self.tweets)):
             temp_col = 0
             ft = Featurizer(self.tweets[i])
-            word_dict, ngrams = generateNgramID(n)
+            word_dict, ngrams = generateNgramID(n, 0)
+
+            for word in ft.tweet: #NOTE DO THIS
+                fm[i][temp_col] = 0
+                temp_col += 1
+
+            fm[i][temp_col] = ft.getAvgEmojis()
+            fm[i][temp_col + 1] = ft.getNumURL()
+            fm[i][temp_col + 2], fm[i][temp_col + 3] = ft.getNumTags()
+            fm[i][temp_col + 4], fm[i][temp_col + 5], fm[i][temp_col + 6], dummy, dummy2 = ft.getPOSTaggedDistribution()
+            fm[i][temp_col + 7] = ft.getNumTokens()
+
+
+    def getStylisticMatrix(self, cols, n):
+        fm = np.zeros( (len(self.tweets)), cols)
+
+        for i in range(len(self.tweets)):
+            temp_col = 0
+            ft = Featurizer(self.tweets[i])
+            word_dict, ngrams = generateNgramID(n, 1)
+
             for word in ft.tweet:
                 fm[i][temp_col] =
                 temp_col += 1
-            fm[i][temp_col] = ft.getAvgEmojis()
-            temp_col += 1
-            fm[i][temp_col] = ft.getNumURL()
-            temp_col += 1
-            fm[i][temp_col], fm[i][temp_col+1] = ft.getNumTags()
-            temp_col += 2
-            fm[i][temp_col], fm[i][temp_col + 1], fm[i][temp_col + 2], dummy, dummy2 = ft.getPOSTaggedDistribution()
 
-
-    def getStylisticMatrix(self):
-        pass
+            fm[i][temp_col] = ft.getAvgNumPunct()
+            fm[i][temp_col + 1] = ft.getAvgWordSize()
+            fm[i][temp_col + 2] = ft.getVocabSize()
+            dummy, dummy1, dummy2, fm[i][temp_col + 3], fm[i][temp_col + 4] = ft.getPOSTaggedDistribution()
+            fm[i][temp_col + 5] = ft.getDigitFrequency()
+            fm[i][temp_col + 6] = ft.getAvgHashTagLength()
+            fm[i][temp_col +7] = ft.getLetterFrequency()
+            fm[i][temp_col + 8] = ft.getAvgCapitalizations()
