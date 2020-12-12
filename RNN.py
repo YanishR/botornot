c58402b357817dd66b6ca92d2e5f91eac7b728bd
lipz0624 <paige.li@richmond.edu>
2020-12-13 01:23:15 +0800

    Clean up RNN and add Readme

--- RNN.py
+++ RNN.py
@@ -1,6 +1,3 @@
-import torch.nn as nn
-import torch
-import gensim
 import gensim.downloader as api
 
 from tqdm import tqdm
@@ -9,63 +6,52 @@ import numpy as np
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten, LSTM, Dropout, Activation, Embedding, Bidirectional
+from tensorflow.keras.layers import Dense, LSTM, Dropout, Activation, Embedding, Bidirectional
 from tensorflow.keras import backend as K
 
-vocab_size = 5000 # make the top list of words (common words)
-embedding_dim = 32
-max_length = 200
-trunc_type = 'post'
-padding_type = 'post'
-oov_tok = '<OOV>' # OOV = Out of Vocabulary
-
-# numpy.random.seed(7)
-
-# glove_model = api.load("glove-twitter-25")  # load glove vectors
-from Featurizer import *
 from dataparser import *
 from tweet import *
 
-
-
-'''
-1. preprocessing words:
-  - We replace occurrences of hashtags, URLs, numbers and user mentions with 
-  the tags “<hashtag>", “<url>", “<number>", or ‘<user>".
-  • Similarly, most common emojis are replaced with “<smile>", “<heart>", 
-  “<lolface>", “<neutralface>" or “<angryface>",depending on the specific emoji.
-  • For words written in upper case letters or for words containing more than 2 
-  repeated letters, a tag denoting that is placed after the occurrence of the word. 
-  For example, the word “HAPPY" would by replaced by two tokens, “happy" and “<allcaps>".
-  • All tokens are converted to lower case.
-2. tokenize
-'''
-
-'''
-1. word embeddings; return vectors
-use vectors to form tensors
-
-'''
+# hyperparameters
+vocab_size = 5000 # make the top list of words (common words)
+embedding_dim = 32 # embedding dimension
+max_length = 200 # tweet max length 
+trunc_type = 'post' # truncate at the end
+padding_type = 'post' # padding at the end
+oov_tok = '<OOV>' # OOV = Out of Vocabulary
 
 class RNN():
+  '''Construct RNN model
+  Input: data  - a list of string (tweets)
+         label - a list of binary number indicating categories
+  '''
   def __init__(self, data, label):
-    self.data = data
-    self.label = label
+    self.data = data 
+    self.label = label 
     self.data_tokenizer = None
     self.model = None
     self.vocab_size = 5000
 
+  '''Initialized the training tokenizer'''
   def initializeTokenizer(self):
     tokenizer = Tokenizer(num_words=self.vocab_size, oov_token=oov_tok)
     tokenizer.fit_on_texts(self.data)
     self.data_tokenizer = tokenizer
 
+  '''Convert string data to number vectors with padding and truncation
+  Input: data - a list of string (tweets)
+  Return: vectors for data - a list of vectors
+  '''
   def convert2vec(self, data):
     sequences = self.data_tokenizer.texts_to_sequences(data)
     padded_seq = pad_sequences(sequences, maxlen=max_length, 
       padding=padding_type, truncating=trunc_type)
     return padded_seq
   
+  ''' Get embedding Metrics from GloVe embeddings
+  Used only for GloVe embeddings
+  Return: embedding matrix
+  '''
   def embeddingMetrics(self):
     glove_model = api.load("glove-twitter-200")  # load glove vectors
     word_vectos = glove_model.wv
@@ -80,7 +66,10 @@ class RNN():
         embedding_matrix[i] = embedding_value
     return embedding_matrix
 
+  '''Train LSTM model'''
   def trainLSTM(self):
+    '''Recall, Precision, F1-Score computation. 
+    Keras removed them from core metrics'''
     def recall_m(y_true, y_pred):
       true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
       possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
@@ -103,26 +92,30 @@ class RNN():
     train_label = np.array(self.label)
     model = Sequential()
     model.add(Embedding(vocab_size, embedding_dim))
-    # embedding_matrix = self.embeddingMetrics()
-    # model.add(Embedding(self.vocab_size, max_length, weights = [embedding_matrix], input_length= max_length, trainable = False))
-    model.add(Dropout(0.2))
-    model.add(Bidirectional(LSTM(100)))
-    model.add(Dense(64, activation='relu'))
-    model.add(Dense(32, activation='relu'))
+    # embedding_matrix = self.embeddingMetrics() # only used for GloVe embeddings
+    # model.add(Embedding(self.vocab_size, max_length, weights = [embedding_matrix], 
+    # input_length= max_length, trainable = False)) # only used for GloVe embeddings
+    # model.add(Dropout(0.2)) # prevent overfitting
+    model.add(Bidirectional(LSTM(32)))
+    model.add(Dense(128, activation='tanh'))
     model.add(Dense(1, activation='sigmoid'))
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy','AUC', f1_m, precision_m, recall_m])
     print(model.summary())
-    model.fit(train_padded, train_label, validation_split=0.2, epochs=3, batch_size=64)
+    model.fit(train_padded, train_label, epochs=1, validation_split=0.2, batch_size=16, verbose=0)
     self.model = model
 
+  '''Predict and Evaluate using trained LSTM model, Print scores.
+  Input: test_data - test set data, a list of string (tweets)
+         test_label - test set label, a list of binary number indicating categories
+  Return: scores - a list of metrics results
+  '''
   def evaluate(self, test_data, test_label):
     test_padded = self.convert2vec(test_data)
     test_label_new = np.array(test_label)
-    scores = self.model.evaluate(test_padded, test_label_new, verbose=1)
-    print(scores)
+    scores = self.model.evaluate(test_padded, test_label_new, verbose=0)
+    # print(scores)
     print("Accuracy: %.2f%%" % (scores[1]*100))
-
-
+    return scores
 
 if __name__ == "__main__":
     print("hello world!")
@@ -131,9 +124,7 @@ if __name__ == "__main__":
 
     tweets = Data(electionTweets, electionTrolls)
     x_train, x_test, y_train, y_test = tweets.getSplitDataDL(.3)
-    # print(x_train[:10])
-    # print(y_train[:10])
-    train_data = []
+    train_data = [] # for preprocess
     test_data = []
     for t in x_train:
       T = Tweet(t)
@@ -141,12 +132,17 @@ if __name__ == "__main__":
     for t in x_test:
       T = Tweet(t)
       test_data.append(T.preprocess())
-    # print(train_data[:10])
-    print(len(train_data), len(y_train))
-    print(len(test_data), len(y_test))
+    # print(len(train_data), len(y_train))
+    # print(len(test_data), len(y_test))
     myRNN = RNN(train_data, y_train)
     myRNN.trainLSTM()
-    myRNN.evaluate(test_data, y_test)
+    scores = myRNN.evaluate(test_data, y_test)
+    print(scores)
+
+
+
+
+
 
 
 
